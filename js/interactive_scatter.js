b83515e35f862b69947eba7594c717627e239c94
Alex Kanellis <akan0033@student.monash.edu>
2023-10-09 19:07:47 +1100

    tw

--- js/interactive_scatter.js
+++ js/interactive_scatter.js
@@ -6,27 +6,46 @@ const spec = {
     "width": 800,
     "height": 400,
     "data": {"url": "https://raw.githubusercontent.com/akanel15/fit3179-hwtask/main/data/world-data-2023.csv"},
-    "transform": [
-    {
-        "calculate": "{'United States': '🇺🇸', 'Canada': '🇨🇦', 'United Kingdom': '🇬🇧', 'Australia': '🇦🇺', 'France': '🇫🇷', 'Germany': '🇩🇪', 'Japan': '🇯🇵', 'China': '🇨🇳', 'India': '🇮🇳', 'Brazil': '🇧🇷', 'Russia': '🇷🇺', 'South Africa': '🇿🇦'}[datum.Country]",
-        "as": "emoji"
-    }
-    ],
-    "mark": {"type": "text", "baseline": "middle"},
-    "encoding": {
-    "y": {"field": "Infant mortality", "type": "quantitative"},
-    "x": {"field": "Physicians per thousand", "type": "quantitative"},
-    "tooltip": [
-    {"field": "Country", "type": "ordinal", "title": "Country"},
-    {"field": "Infant mortality", "type": "quantitative", "title": "Infant mortality"}
-    ],
-    "text": {"field": "emoji", "type": "nominal"},
-    }
-}  
+
+    "layer": [
+        {
+          "mark": {"type": "point", "filled": true},
+          "encoding": {
+            "y": {"field": "Infant mortality", "type": "quantitative"},
+            "x": {"field": "Physicians per thousand", "type": "quantitative"},
+            "tooltip": [
+              {"field": "Country", "type": "ordinal", "title": "Country"},
+              {"field": "Infant mortality", "type": "quantitative", "title": "Infant mortality"}
+            ]
+          }
+        },
+        {
+          "mark": {"type": "text", "baseline": "middle"},
+          "transform": [
+            {
+                "calculate": "{'United States': '🇺🇸', 'Canada': '🇨🇦', 'United Kingdom': '🇬🇧', 'Australia': '🇦🇺', 'France': '🇫🇷', 'Germany': '🇩🇪', 'Japan': '🇯🇵', 'China': '🇨🇳', 'India': '🇮🇳', 'Brazil': '🇧🇷', 'Russia': '🇷🇺', 'South Africa': '🇿🇦'}[datum.Country]",
+                "as": "emoji"
+            }
+            ],
+            "mark": {"type": "text", "baseline": "middle"},
+            "encoding": {
+            "y": {"field": "Infant mortality", "type": "quantitative"},
+            "x": {"field": "Physicians per thousand", "type": "quantitative"},
+            "tooltip": [
+            {"field": "Country", "type": "ordinal", "title": "Country"},
+            {"field": "Infant mortality", "type": "quantitative", "title": "Infant mortality"}
+            ],
+            "text": {"field": "emoji", "type": "nominal"},
+            }
+        }
+      ]
+    };
 
 xAxisSelect.addEventListener("change", () => {
 const selectedOption = xAxisSelect.value;
-spec.encoding.x.field = selectedOption;    
+spec.layer[0].encoding.x.field = selectedOption; 
+spec.layer[0].encoding.x.title = selectedOption;
+spec.layer[1].encoding.x.field = selectedOption;  
 vegaEmbed("#vis", spec, {mode: "vega-lite"}).then(console.log).catch(console.warn);
 
 });
